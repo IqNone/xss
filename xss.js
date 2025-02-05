c87a08f42fc16e47956d46d05268bfc622380a68
Adrian Iriciuc <adrian.iriciuc@aurea.com>
2025-02-05 13:27:00 +0200

    fixes

--- xss.js
+++ xss.js
@@ -1,8 +1,9 @@
 const img = document.createElement("img");
 img.setAttribute('src', 'https://raw.githubusercontent.com/IqNone/xss/refs/heads/main/giphy.gif');
-img.style["position"] = "absolute";
-img.style["top"] = "200px";
+img.style["position"] = "sticky";
+img.style["top"] = "0";
 img.style["left"] = "10px";
+img.style["z-indez"] = "999999999";
 document.body.appendChild(img);
 
 document.body.insertAdjacentHTML('beforeend',
